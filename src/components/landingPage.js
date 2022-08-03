65e2b9715bb641cef3d27241ab2656be83ec51b9
Kristine Kochie <kristinekochie@gmail.com>
2022-08-03 16:22:49 -0400

    Task checkbox

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,30 +1,26 @@
 // import { useEffect, useState } from 'react'
-import styled from 'styled-components'
-
-import NavFormBar from './navFormBar'
-import ProjectCard from './projectCard'
+import styled from "styled-components";
+import ProjectCard from "./projectCard";
 
 const Container = styled.div`
+  .projectItem {
+  }
 
-.projectItem{ }
+  .bttn {
+  }
+`;
 
-.bttn{ }
-
-`
-
-
-function LandingPage(){
-    return (
-        <Container>
-            <h1> Project Manager </h1>
-            {/* potentially a search bar here by Project name, teammembers, languages used */}
-            <div class="projectItems"> 
-                <ProjectCard/>
-                <button class="bttn"> Delete Project </button>
-            </div>
-            <button class="bttn"> Create New Project </button>
-
-        </Container>
-    )
+function LandingPage() {
+  return (
+    <Container>
+      <h1> Project Manager </h1>
+      {/* potentially a search bar here by Project name, teammembers, languages used */}
+      <div class="projectItems">
+        <ProjectCard />
+        <button class="bttn"> Delete Project </button>
+      </div>
+      <button class="bttn"> Create New Project </button>
+    </Container>
+  );
 }
-export default LandingPage
+export default LandingPage;
