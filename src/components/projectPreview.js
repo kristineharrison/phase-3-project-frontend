1964d00c2c2265f712ba21b8587e5db08bda7106
Isabella Vieira <isabella.q.vieira@gmail.com>
2022-08-04 10:52:44 -0400

    this

--- src/components/projectPreview.js
+++ src/components/projectPreview.js
@@ -4,24 +4,11 @@ const Container = styled.div`
 
 `
 
-<<<<<<< HEAD
-function ProjectCard({eachProject}){
-
-
-    return(
-        <Container>
-            <h3>{eachProject.name}</h3>
-            <img src={eachProject.business.logo_url} 
-                style={{width: '300px'}} 
-                alt={eachProject.business.name} 
-                />
-=======
 function ProjectPreview({eachProject}) {
     return(
         <Container>
             <h3>{eachProject.name}</h3>
             <img src={eachProject.business.logo_url} style={{width: '300px'}} alt={eachProject.business.name} />
->>>>>>> kristine_branch
             <h6>{eachProject.due_date}</h6>
             {/* on click upens this information */}
             <p>{eachProject.business.name}</p>
