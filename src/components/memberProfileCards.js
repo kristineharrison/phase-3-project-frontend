78bf2d9de495d7c396d7f641bd4db57eafbba640
Isabella Vieira <isabella.q.vieira@gmail.com>
2022-08-04 19:37:41 -0400

    added some styling

--- src/components/memberProfileCards.js
+++ src/components/memberProfileCards.js
@@ -1,6 +1,23 @@
+// colors to use
+//  white for main background
+//  bluish for accents (#75B9BE)
+//  charcole for text (383F51)
+//  silver pink for background accents (#D4BEBE)
+
 import styled from "styled-components";
 
 const Container = styled.div`
+  color: white;
+  border: solid 3px #75b9be;
+  border-radius: 20px / 10px;
+  background-color: #D4BEBE;
+  width: 700px;
+  height: 100px;
+  margin: 10px;
+
+  .not-projects{
+  }
+
   .project-list {
   }
 `;
@@ -8,9 +25,10 @@ const Container = styled.div`
 function MemberProfileCard({eachMember}){
   return(
       <Container>
+        <div className="not-projects">
           <p> Name: {eachMember.name} </p>
           <p> Skill: {eachMember.skill}</p>
-          <br/> 
+          </div>
           <div class="projects-list">
               <li> "List of projects theyre on" </li>
           </div>
--- src/components/membersLandingPage.js
+++ src/components/membersLandingPage.js
@@ -1,9 +1,23 @@
+// colors to use
+//  white for main background
+//  bluish for accents (#75B9BE)
+//  charcole for text (383F51)
+//  silver pink for background accents (#D$B3B3)
+
 import styled from "styled-components";
 import MemberProfileCard from "./memberProfileCards";
 
 const Container = styled.div`
-  .member-cards {
+
+  display: flex;
+  /* align-items: center; */
+  justify-content: center;
+  header{
+    text-decoration: underline;
+
   }
+
+
 `;
 
 function MembersHomePage({sendMembersData}){
@@ -14,8 +28,12 @@ function MembersHomePage({sendMembersData}){
       );
   return(
       <Container>
+          <header>
           <h1> Members Manager</h1>
+          </header>
+          <div className="cards">
           {membersList}
+          </div>
 
 
       </Container>
