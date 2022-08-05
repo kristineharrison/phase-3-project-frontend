031ce4c9a9d2fc8f2915117cb9338f466cbb3718
Isabella Vieira <isabella.q.vieira@gmail.com>
2022-08-04 21:06:37 -0400

    fixed landing page error

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -8,7 +8,17 @@ const Container = styled.div`
   flex-flow: row wrap;
   padding: 20px;
 
-function LandingPage({sendProjectsData, tasksData, functionToDeleteProjects, functionToDeleteTasks,}){
+  function LandingPage({
+  sendProjectsData,
+  tasksData,
+  functionToDeleteProjects,
+  functionToDeleteTasks,
+}) {
+  const [openFullProject, setOpenFullProject] = useState(true);
+
+  function displayFullCards() {
+    setOpenFullProject(!openFullProject);
+  }
 
   const projectList = sendProjectsData.map((eachProject) => (
     <ProjectPreview
@@ -20,12 +30,12 @@ function LandingPage({sendProjectsData, tasksData, functionToDeleteProjects, fun
     />
   ));
 
-  return(
+  return (
     <div>
       <h1> Project Manager </h1>
       <Container>{projectList}</Container>
       <button className="bttn"> Create New Project </button>
     </div>
-  )
+  );
 }
-export default LandingPage
+export default LandingPage;
